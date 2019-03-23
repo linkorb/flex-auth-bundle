e3b5342cd70d792f6a850230fc00f34af647c5b4
Aleksandr <sashaaro@gmail.com>
2019-03-23 18:13:26 +0300

    remove exces code s

--- src/FlexUserProviderFactory.php
+++ src/FlexUserProviderFactory.php
@@ -9,7 +9,6 @@ use Symfony\Component\Config\Definition\Builder\NodeDefinition;
 use Symfony\Component\DependencyInjection\ChildDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
-use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
 
 /**
  * Class FlexUserUserProviderFactory
@@ -35,17 +34,6 @@ class FlexUserProviderFactory implements UserProviderFactoryInterface
         $definition->setFactory([AuthFlexTypeProviderFactory::class, 'fromEnv']);
         $definition->addArgument($config['env_var']);
         $container->setDefinition(AuthFlexTypeProviderInterface::class, $definition);
-
-        if ($config['flex_password_encoder_enable']) {
-            if (!$container->hasAlias(EncoderFactoryInterface::class)) {
-                throw new \InvalidArgumentException(
-                    sprintf('Try replace general password encoder to flex encoder. "%s" service is not defined. Set "flex_encoder_enable" as false', EncoderFactoryInterface::class)
-                );
-            }
-
-            $container->removeAlias(EncoderFactoryInterface::class);
-            $container->setAlias(EncoderFactoryInterface::class, FlexAuthPasswordEncoder::class);
-        }
     }
 
     public function getKey()
